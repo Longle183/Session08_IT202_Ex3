6a302a3d3eab023d94281914a036c50d9ae604f1
Longle183 <lethanhlong1224@gmail.com>
2026-05-24 21:41:49 +0700

    Delete ex3/ex3_ss8_it211-main directory

--- ex3/ex3_ss8_it211-main/src/main/java/com/restaurant/ex3/repository/TransactionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.restaurant.ex3.repository;
-
-import com.restaurant.ex3.entity.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransactionRepository
-        extends JpaRepository<Transaction, Long> {
-}
